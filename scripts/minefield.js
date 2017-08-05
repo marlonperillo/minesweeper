86ba64f415068fafd6a1ae882329232305d1de59
marlonperillo <m3perillo@gmail.com>
2017-08-06 02:04:57 +0900

    added random mine generator

--- scripts/minefield.js
+++ scripts/minefield.js
@@ -1,22 +1,39 @@
 
-initialize(10, 3)
 
-function initialize(height, width){
+height = 10
+width = 3
+percent = 0.23
 
-	var minefield_object = document.getElementById("minefield")
+var minefieldObject = document.getElementById("minefield")
+initializeTableMineField(height, width, minefieldObject)
+mines = generateMines(height, width, percent)
 
-	setupmine(height, width, minefield_object)
 
+
+function generateMines(height, width, percent){
+
+	var list = new Array()
+
+	var minesCount = parseInt( height * width * percent)
+
+	for(count = 0; count < minesCount; ){
+		randomRow = parseInt(Math.random() * height)
+		randomWidth = parseInt(Math.random() * width)
+		if(list.indexOf(randomRow + ',' + randomWidth) == -1){
+			list.push(randomRow + ',' + randomWidth)
+			count++
+		}
+	}
+	return list
 
 }
 
-function setupmine(height, width, tableObject){
+function initializeTableMineField(height, width, tableObject){
 
 	for( i = 0; i < height; i++){
 		newrow = tableObject.insertRow(i)
 		for( j = 0; j< width; j++){
 			cell = newrow.insertCell(j)
-			cell.innerHTML = "[r" + i + "|c" + j +"]"
 			cell.onclick = function(var1,var2) {
 				return function(){
 					stepOn(var1, var2)
